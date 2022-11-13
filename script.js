26de3506f93a14d25f8ef200014756ebd4fa4eb6
k.sudharshan <kamavaramsudharshan1706@gmail.com>
2022-11-13 14:26:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const heading = document.createElement('h1');
+const headingTagls = document.createElement('h1');
+document.body.style.textAlign = 'center';
+document.body.style.backgroundColor = 'bisque';
+document.body.style.marginTop = '250px';
+document.body.style.fontSize = "x-large"
+document.body.append(heading);
+document.body.append(headingTagls);
+var countDown = 10;
+setTimeout(() => {
+    heading.innerText = '';
+    heading.innerText = countDown;
+    countDown--;
+    setTimeout(() => {
+        heading.innerText = '';
+        heading.innerText = countDown;
+        countDown--;
+        setTimeout(() => {
+            heading.innerText = '';
+            heading.innerText = countDown;
+            countDown--;
+            setTimeout(() => {
+                heading.innerText = '';
+                heading.innerText = countDown;
+                countDown--;
+                    setTimeout(() => {
+                    heading.innerText = '';
+                    heading.innerText = countDown;
+                    countDown--;
+                        setTimeout(() => {
+                        heading.innerText = '';
+                        heading.innerText = countDown;
+                        countDown--;
+                        setTimeout(() => {
+                            heading.innerText = '';
+                            heading.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                            heading.innerText = '';
+                            heading.innerText = countDown;
+                            countDown--;
+                            setTimeout(() => {
+                                heading.innerText = '';
+                                heading.innerText = countDown;
+                                countDown--;
+                                setTimeout(() => {
+                                    heading.innerText = '';
+                                    heading.innerText = countDown;
+                                    countDown--;
+                                    setTimeout(() => {
+                                        heading.innerText = '';
+                                        headingTagls.innerText = 'Happy Independence Day!';
+                                    countDown--;
+                                }, 1000);
+                            }, 1000);
+                            }, 1000);
+                        }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+
